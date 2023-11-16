25e9627d5c71f5f193a45b8f903e811dcbfc665f
Maya <foenx0315@gmail.com>
2023-11-16 19:19:51 +0530

    Stuff

---
+++ App/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        matteBlack: "#101010",
+        secondaryBg: "#282828",
+        primaryText: "",
+        lightText: "#929191",
+        cta: "#FAF906",
+        borderColor: "#353131f6",
+      },
+      backgroundImage: {
+        "threads-bg": "url('/assets/threadsbg.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
